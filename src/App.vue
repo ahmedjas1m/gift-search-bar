e089c15ed9ed517cf95404bf395085a9ce58425b
Ahmed Jasim <itsahmedjasim@gmail.com>
2024-06-25 11:05:15 +0300

    completed ✅

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
 import { ref, watch } from 'vue'
 
+const products = ref([])
 const searchTerm = ref('')
 
-const findProducts = async term => {}
+const isLoading = ref(false)
+
+function debounce(ck) {
+  let timer
+  return () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      ck()
+    }, 300)
+  }
+}
+
+const createDebounce = debounce(async term => {
+  try {
+    if (term.length === 0) return (products.value = [])
+    isLoading.value = true
+    const apiResponseBody = await (await fetch(`https://dummyjson.com/products/search?q=${term}&limit=5`)).json()
+    products.value = apiResponseBody.products
+  } catch (error) {
+    console.error(error)
+    alert('Holy cow!')
+  } finally {
+    isLoading.value = false
+  }
+})
+
+const findProducts = term => createDebounce(term)
 
 watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
@@ -12,8 +39,9 @@ watch(searchTerm, newTerm => findProducts(newTerm))
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
+    <p v-if="isLoading" class="text-lg text-center">loading...</p>
+    <ul v-else-if="products.length > 0">
+      <li v-for="product in products" :key="product.id">{{ product.title }} - ${{ product.price }}</li>
     </ul>
   </div>
 </template>
